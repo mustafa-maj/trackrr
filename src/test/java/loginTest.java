de3bcb49a6a0acdea4bc47c321f3dcd7e62591da
mustafa-maj <aliyarbey606@gmail.com>
2024-09-03 11:58:32 +0500

    added junit dependencies and test files

---
+++ src/test/java/loginTest.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class loginTest {
+
+    @Test
+    public void testSomeFunctionality() {
+        // Arrange
+        int expected = 5;
+
+        // Act
+        int actual = 2 + 3;
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSomeFunctionality2() {
+        // Arrange
+        int expected = 5;
+
+        // Act
+        int actual = 2 + 3;
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+}
